d26e9119d088b5b92692cd3961fc8340a66a2dbb
colm-kilduff <colm.kilduff@outlook.com>
2024-11-14 21:47:42 -0800

    writing chanterelle observations and visualising.

--- code/202411-chanterelle-map/202411-chanterelle-map.R
+++ code/202411-chanterelle-map/202411-chanterelle-map.R
@@ -1,37 +1,106 @@
-library(aws.s3)
-WRITE_FROM_S3 <- TRUE
+library(aws.s3); library(R.utils); library(data.table); library(dplyr); library(sf); library(ggplot2)
+WRITE_FROM_S3 <- FALSE
 PROJ_CRS <- 4326
-
+CREATE_CHANTERELLE_OBSERVATIONS <- FALSE
 # In this analysis, we will be downloading the inaturalist observation and taxa datasets.
 # We will then filter for Pacific golden chanterelle (Cantherellus formosus).
-
+filter <- dplyr::filter
 
 if (WRITE_FROM_S3){
   
-  s3_bucket <- aws.s3::get_bucket('s3://inaturalist-open-data')
-  
-  aws.s3::get_bucket_df(s3_bucket, 'taxa')
+  s3_bucket <- aws.s3::get_bucket('inaturalist-open-data')
   
   taxa_object <- aws.s3::get_object('taxa.csv.gz', s3_bucket)
   
-  observation_object <- aws.s3::get_object('observations.csv.gz', s3_bucket)
+  obs_file_path <- aws.s3::save_object('observations.csv.gz', s3_bucket, file.path('./code/202411-chanterelle-map/data', 'observations.csv.gz'))
+  
+  obs_file_path |> 
+    gunzip()
   
   taxa_file_path <- file.path(Sys.getenv('PROJ_DIR'), "code", "202411-chanterelle-map", "data", "taxa.csv.gz")
   
-  observations_file_path <- file.path(Sys.getenv('PROJ_DIR'), "code", "202411-chanterelle-map", "data", "observations.csv.gz")
-  
   writeBin(taxa_object, taxa_file_path)
-  
-  writeBin(observation_object, observations_file_path)
   
 }
 
+observations_csv <- file.path(Sys.getenv('PROJ_DIR'), "code", "202411-chanterelle-map", "data", "observations.csv")
+taxa_csv <- file.path(Sys.getenv('PROJ_DIR'), "code", "202411-chanterelle-map", "data", "taxa.csv")
+
+cantherellus_formosus_taxon <-
+  fread(taxa_csv) |>
+  dplyr::filter(name == 'Cantharellus formosus') |>
+  as_tibble()
+
+if (CREATE_CHANTERELLE_OBSERVATIONS){
+  
+  chunk_size <- 10000000
+  
+  filtered_chunks <- list()
+  
+  row_start <- 1
+  
+  header <- fread(observations_csv, nrows = 0)
+  
+  # initialise chunk so that the while loop starts
+  chunk <- list()
+  
+  while(!is.null(chunk)) {
+    
+  chunk <- tryCatch({
+    fread(observations_csv, skip = row_start, nrows = chunk_size)
+  },
+  error = function(e) {
+    message("An error occurred: ", e)
+    NULL  # Return NULL value if an error occurs
+  })
+  
+  if (!is.null(chunk)){
+    
+  if (nrow(chunk) == 0) break
+  
+  setnames(chunk, names(header))
+  
+  filtered_chunk <- 
+    chunk |>
+    dplyr::filter(taxon_id %in% cantherellus_formosus_taxon$taxon_id)
+  
+  if (nrow(filtered_chunk) > 0) {
+    filtered_chunks <- append(filtered_chunks, list(filtered_chunk))
+  }
+  
+  print(glue('{row_start}_to_{row_start + chunk_size}'))
+  
+  # Move to the next chunk
+  row_start <- row_start + chunk_size
+  
+  }
+  
+  }
+  
+  chanterelle_observations <- bind_rows(filtered_chunks)
+  
+  chanterelle_observations |>
+    write.csv('./code/202411-chanterelle-map/data/chanterelle_observations.csv')
+  
+}
+
+chanterelle_observations <- 
+  read.csv('./code/202411-chanterelle-map/data/chanterelle_observations.csv') |>
+  filter(!is.na(latitude), !is.na(longitude), quality_grade != 'needs_id') |>
+  as_tibble() |>
+  st_as_sf(coords = c('longitude', 'latitude')) |>
+  st_set_crs(4326)
+
+# plotting the observations
+
+ggplot() +
+  # add a background map for this
+  geom_sf(data = chanterelle_observations)
 
 # Now we can read in the observations and taxa datasets.
 # We will use fread to first identify the row in taxa with Cantherellus formosus
 
 #TODO: Get and read in elevation data
-#TODO: Iterate through observations to filter only Cantherellus formosis
 #TOOD: Download and read in NRCAN dataset
 #TODO: Download and read in BC shapefile.
 #TODO: Intersect all datasets and model using a gam with a thin-plate spline on lat long
